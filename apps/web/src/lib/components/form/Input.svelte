d96dd912dbe6105d6bfd41e256547d7a721836f2
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2022-10-13 09:09:09 +0200

    fix input component

--- apps/web/src/lib/components/form/Input.svelte
+++ apps/web/src/lib/components/form/Input.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
+	import Field from './Field.svelte';
 	export let value: string = '';
 	export let placeholder: string = '';
 	export let id: string;
 	export let label: string;
 	export let type: string;
 	export let disabled: boolean = false;
+	export let required: boolean = false;
+	export let dark: boolean;
 </script>
 
-<div class="field">
+<Field>
 	<label for={id}>{label}</label>
-	<input {disabled} {id} name={id} {type} {value} {placeholder} />
-</div>
+	<input {required} {disabled} {id} name={id} {type} {value} {placeholder} />
+</Field>
 
 <style lang="postcss">
-	.field {
-		display: grid;
-		gap: var(--size-1);
-	}
-
 	input {
 		background: var(--gray-2);
 		color: var(--gray-9);
