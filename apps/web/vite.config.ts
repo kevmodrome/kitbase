3215d449fad9f2c74d125548db2edeb2af0cf58e
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2022-10-13 09:07:00 +0200

    ...and the config file 🤦‍♂️

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import Icons from 'unplugin-icons/vite'
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), Icons({
+		compiler: 'svelte',
+	  })]
 };
 
 export default config;
