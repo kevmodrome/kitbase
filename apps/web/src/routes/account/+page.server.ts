d88abee76d2e8865110fba1a1e74ce6aa2c8215d
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2022-10-21 14:31:35 +0200

    remove log

--- apps/web/src/routes/account/+page.server.ts
+++ apps/web/src/routes/account/+page.server.ts
@@ -1,11 +1,10 @@
 import type { Actions } from './$types';
 import type { RequestEvent } from '@sveltejs/kit'
+import { applyAction } from '$app/forms';
 
 export const actions: Actions = {
     update: async ({ cookies, locals, request }: RequestEvent) => {
         const data = await request.formData();
-
-        console.log(locals.user)
 
         const updatedProfile = await locals.pb.records.update("profiles", locals.user.profile.id, { name: data.get('name') });
 
